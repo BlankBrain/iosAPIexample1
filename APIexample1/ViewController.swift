66cea4a3c0ac3d87235e99de32165931ec5e6444
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-01-13 16:26:41 +0600

    #API_ADDED

--- APIexample1/ViewController.swift
+++ APIexample1/ViewController.swift
@@ -12,16 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var getTextview: UITextView!
     
+    let session = URLSession.shared
+    let url = URL(string: "https://api.darksky.net/forecast/e03fba5c687227d65dd03e52b231c81d/37.8267,-122.4233")!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+
 
     @IBAction func getRequestButtonPressed(_ sender: Any) {
         
+        let task = session.dataTask(with: url) { data, response, error in
+            print(data)
+            print(response)
+            print(error)
         
+        }
+        task.resume()
     }
     
 }
